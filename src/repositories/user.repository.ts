64bf4f14fccd0f401af5cffa7ce8744f49d0cc32
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-08 20:19:03 -0300

    fix: return user insert object

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,7 +3,7 @@ import type { NewUser } from "@/lib/database/schema/public/User";
 
 export class UserRepository {
 	async create(data: NewUser) {
-		await db.insertInto("user").values(data).execute();
+		return await db.insertInto("user").values(data).returningAll().execute();
 	}
 
 	async findByEmail(email: string) {
