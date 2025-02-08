7991d186bb3d055728c9b91fa98dec3247ee2eae
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-07 21:28:59 -0300

    lint: run biome check

--- kysely.config.ts
+++ kysely.config.ts
@@ -4,12 +4,12 @@ import { defineConfig, getKnexTimestampPrefix } from "kysely-ctl";
 import { Pool } from "pg";
 
 export default defineConfig({
-  dialect: "pg",
-  dialectConfig: {
-    pool: new Pool({ connectionString: process.env.DATABASE_URL }),
-  },
-  migrations: {
-    migrationFolder: "src/lib/database/migrations",
-    getMigrationPrefix: getKnexTimestampPrefix,
-  }
+	dialect: "pg",
+	dialectConfig: {
+		pool: new Pool({ connectionString: process.env.DATABASE_URL }),
+	},
+	migrations: {
+		migrationFolder: "src/lib/database/migrations",
+		getMigrationPrefix: getKnexTimestampPrefix,
+	},
 });
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,20 +1,20 @@
-import "dotenv/config"
-import { z } from "zod"
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "prod"]).default("dev"),
-  DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string(),
-  TZ: z.string().default("UTC"),
-  PORT: z.coerce.number().default(3000)
-})
+	NODE_ENV: z.enum(["dev", "prod"]).default("dev"),
+	DATABASE_URL: z.string().url(),
+	JWT_SECRET: z.string(),
+	TZ: z.string().default("UTC"),
+	PORT: z.coerce.number().default(3000),
+});
 
-const _env  = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
-if(!_env.success) {
-  console.error("invalid enviroment variables", _env.error.format());
+if (!_env.success) {
+	console.error("invalid enviroment variables", _env.error.format());
 
-  throw new Error("invalid enviroment variables")
+	throw new Error("invalid enviroment variables");
 }
 
-export const env = _env.data
+export const env = _env.data;
