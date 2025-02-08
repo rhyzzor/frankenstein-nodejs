f726c990cb123a339f8afd7606a39813c9aa01d8
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-07 21:02:07 -0300

    feat: add env validation

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"
+import { z } from "zod"
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "prod"]).default("dev"),
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  TZ: z.string().default("UTC"),
+  PORT: z.coerce.number().default(3000)
+})
+
+const _env  = envSchema.safeParse(process.env)
+
+if(!_env.success) {
+  console.error("invalid enviroment variables", _env.error.format());
+
+  throw new Error("invalid enviroment variables")
+}
+
+export const env = _env.data
