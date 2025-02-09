034890a77f73e38229dea7660d249bd22fcc12f6
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-08 22:46:09 -0300

    test: ensure that the user can be deleted

---
+++ src/use-cases/test/unit/delete-user.spec.ts
@@ -0,0 +1,13 @@
+import type { UserId } from "@/lib/database/schema/public/User";
+import { DeleteUserUseCase } from "@/use-cases/delete-user";
+import { mockUserRepository } from "@/utils/test/mock-user-repository";
+
+const sut = new DeleteUserUseCase(mockUserRepository);
+
+describe("DeleteUserUseCase", () => {
+	it("should be able to delete user", async () => {
+		await sut.execute({ userId: 1 as UserId });
+
+		expect(mockUserRepository.delete).toHaveBeenCalledTimes(1);
+	});
+});
