ebf14c6106f85ee58ef04cb1b6db13abff9312d4
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-09 08:26:20 -0300

    feat: configure swagger with zod provider

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
+import fastifySwagger from "@fastify/swagger";
 import fastify from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
 import { env } from "./env";
 
 export const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "User API",
+			description: "User API documentation",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
