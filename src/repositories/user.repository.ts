ceae3f9e2cefb4f428c260f0f222dac70c05ad8a
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-08 22:19:03 -0300

    feat: implements a method to get user by id

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/database/kysely";
-import type { NewUser } from "@/lib/database/schema/public/User";
+import type { NewUser, UserId } from "@/lib/database/schema/public/User";
 
 export class UserRepository {
 	async create(data: NewUser) {
@@ -8,6 +8,16 @@ export class UserRepository {
 			.values(data)
 			.returningAll()
 			.executeTakeFirstOrThrow();
+	}
+
+	async findById(id: UserId) {
+		const user = await db
+			.selectFrom("user")
+			.selectAll()
+			.where("id", "=", id)
+			.executeTakeFirst();
+
+		return user;
 	}
 
 	async findByEmail(email: string) {
