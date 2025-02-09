ef4877094dac66d44178fc9bb39be33664566b91
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-09 08:17:24 -0300

    feat: configure refresh token cookie

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { env } from "./env";
@@ -6,10 +7,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: "refreshToken",
+		signed: false,
+	},
 	sign: {
 		expiresIn: "10m",
 	},
 });
+
+app.register(fastifyCookie);
 
 app.get("/", () => {
 	return { message: "hello world" };
