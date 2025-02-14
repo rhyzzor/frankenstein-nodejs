082648777e81c0fdf27cd118e8eaa58724a36f76
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-14 10:34:47 -0300

    feat: implements a error handler to zod validation

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import {
 	serializerCompiler,
 	validatorCompiler,
 } from "fastify-type-provider-zod";
+import { ZodError } from "zod";
 import { env } from "./env";
 import { routes } from "./http/controllers/routes";
 
@@ -48,4 +49,20 @@ app.register(routes, { prefix: "/api" });
 
 app.get("/", () => {
 	return { message: "hello world" };
+});
+
+app.setErrorHandler((error, _, reply) => {
+	if (error instanceof ZodError) {
+		return reply
+			.status(400)
+			.send({ message: "Validation error", issues: error.format() });
+	}
+
+	if (env.NODE_ENV !== "prod") {
+		console.error(error);
+	} else {
+		// TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+	}
+
+	return reply.status(500).send({ message: "Internal server error" });
 });
