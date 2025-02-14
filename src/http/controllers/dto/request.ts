aa4ab56766539f5fcdb711fb2c7fcaf6bd526da9
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-14 10:48:48 -0300

    feat: create a register schema validation

--- src/http/controllers/dto/request.ts
+++ src/http/controllers/dto/request.ts
@@ -6,3 +6,11 @@ export const signInSchema = z.object({
 });
 
 export type signInRequest = z.infer<typeof signInSchema>;
+
+export const registerSchema = z.object({
+	name: z.string(),
+	email: z.string().email(),
+	password: z.string().min(6),
+});
+
+export type registerRequest = z.infer<typeof registerSchema>;
