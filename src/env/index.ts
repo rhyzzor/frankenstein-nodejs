fbb97684ede3e3388f56ce37a2ce6977eac3c103
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-14 16:02:09 -0300

    fix: add test env to zod schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["dev", "prod"]).default("dev"),
+	NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
 	DATABASE_URL: z.string().url(),
 	JWT_SECRET: z.string(),
 	TZ: z.string().default("UTC"),
