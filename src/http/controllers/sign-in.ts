d8c1fcdaf7d92602f8c8d817cd65d28c0090de87
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-14 10:38:26 -0300

    refactor: add try catch and compare error

--- src/http/controllers/sign-in.ts
+++ src/http/controllers/sign-in.ts
@@ -1,3 +1,4 @@
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { makeAuthenticateUse } from "@/use-cases/factories/make-authenticate-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import type { signInRequest } from "./dto/request";
@@ -8,36 +9,42 @@ export async function signIn(
 ) {
 	const { email, password } = request.query;
 
-	const authenticateUseCase = makeAuthenticateUse();
+	try {
+		const authenticateUseCase = makeAuthenticateUse();
 
-	const { user } = await authenticateUseCase.execute({ email, password });
+		const { user } = await authenticateUseCase.execute({ email, password });
 
-	const token = await reply.jwtSign(
-		{},
-		{
-			sign: {
-				sub: user.id,
+		const token = await reply.jwtSign(
+			{},
+			{
+				sign: {
+					sub: user.id,
+				},
 			},
-		},
-	);
+		);
 
-	const refreshToken = await reply.jwtSign(
-		{},
-		{
-			sign: {
-				sub: user.id,
-				expiresIn: "7d",
+		const refreshToken = await reply.jwtSign(
+			{},
+			{
+				sign: {
+					sub: user.id,
+					expiresIn: "7d",
+				},
 			},
-		},
-	);
+		);
 
-	return reply
-		.setCookie("refreshToken", refreshToken, {
-			path: "/",
-			secure: true,
-			sameSite: true,
-			httpOnly: true,
-		})
-		.status(200)
-		.send({ token });
+		return reply
+			.setCookie("refreshToken", refreshToken, {
+				path: "/",
+				secure: true,
+				sameSite: true,
+				httpOnly: true,
+			})
+			.status(200)
+			.send({ token });
+	} catch (error) {
+		if (error instanceof InvalidCredentialsError) {
+			return reply.status(400).send({ message: error.message });
+		}
+	}
 }
